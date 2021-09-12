62d01324149f25f8c4ad65c8e9102bb8aa147db9
Rage <zuppin@usi.ch>
2021-09-12 11:25:51 +0200

    Update install_dependecies.sh

--- install_dependecies.sh
+++ install_dependecies.sh
@@ -5,4 +5,7 @@ killall SpringBoard
 sleep 15
 npm i
 killall SpringBoard
+activator send libactivator.system.homebutton
+activator send libactivator.system.homebutton
+
 echo 'Done! Start the server by running: \n node app.js \n and then connect on port 3000'
